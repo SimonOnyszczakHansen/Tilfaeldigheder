b2f74f24e0ccd1d87ce7b18ac869873d8a49f6d5
Simon <Simonhansen0811@zbc.dk>
2024-02-26 16:34:30 +0100

    Done

---
+++ Tilfældihederr/Encrypter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilfældihederr
+{
+    public static class Encrypter
+    {
+        private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+
+        public static string Encrypt(string text)
+        {
+            var encrypted = "";
+            foreach (char c in text.ToUpper()) 
+            {
+                if (Alphabet.Contains(c))
+                {
+                    int index = (Alphabet.IndexOf(c) + 1);
+                    encrypted += Alphabet[index];
+                }
+                else
+                {
+                    encrypted += c;
+                }
+            }
+            return encrypted;
+        }
+        public static string Decrypt(string text)
+        {
+            var decrypted = "";
+
+            foreach (char c in text) 
+            {
+                if (Alphabet.Contains(c))
+                {
+                    int index = (Alphabet.IndexOf(c) - 1 + Alphabet.Length) % Alphabet.Length;
+                    decrypted += Alphabet[index];
+                }
+                else
+                {
+                    decrypted += c;
+                }
+            }
+            return decrypted;
+        }
+    }
+}
--- Tilfældihederr/Program.cs
+++ Tilfældihederr/Program.cs
@@ -1,56 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
+using Tilfældihederr;
 
-namespace Tilfældihederr
+namespace Tilfældighedstest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> randomNumber = RandomTest();
-            List<int> rngNumbers = RandomTest();
+            List<int> randomNumbers = new List<int>();
+            List<int> rngNumbers = new List<int>();
 
-            foreach (int number in randomNumber)
+            Console.WriteLine();
+            RandomTest();
+            foreach (int number in randomNumbers)
             {
-               Console.WriteLine($"Random number: {number}");
+                Console.WriteLine($"Random numbers using Random: {number}");
             }
 
-
+            RandomNumberGeneratorCreateTest();
             foreach (int number in rngNumbers)
             {
-                Console.WriteLine($"rng number: {number}");
+                Console.WriteLine($"Random numbers using RandomNumberGenerator: {number}");
             }
+
+            Console.WriteLine("Benchmarking Random vs RandomNumberGenerator");
+            RunBenchmark(RandomTest, nameof(RandomTest));
+            RunBenchmark(RandomNumberGeneratorCreateTest, nameof(RandomNumberGeneratorCreateTest));
+
+            string originalText = "Hello, my name is Simon";
+            string encryptedText = Encrypter.Encrypt(originalText);
+            string decryptedText = Encrypter.Decrypt(encryptedText);
+
+            Console.WriteLine($"Original text: {originalText}");
+            Console.WriteLine($"Encrypted text: {encryptedText}");
+            Console.WriteLine($"Decrypted text: {decryptedText}");
+
 
             Console.ReadLine();
         }
 
-        static List<int> RandomTest() 
+        static void RandomTest()
         {
             Random random = new Random();
             List<int> numbers = new List<int>();
 
-            for (int i = 0; i < 100; i++) 
+            for (int i = 0; i < 1000000; i++)
             {
                 numbers.Add(random.Next(0, 1000));
             }
-            return numbers;
         }
-        static List<int> RandomNumberGeneratorCreateTest()
+
+        static void RandomNumberGeneratorCreateTest()
         {
             List<int> numbers = new List<int>();
-            for (int i = 0;i < 100;i++) 
+            using (var rng = RandomNumberGenerator.Create())
             {
                 byte[] randomBytes = new byte[4];
-                using (var rng = RandomNumberGenerator.Create())
+                for (int i = 0; i < 1000000; i++)
                 {
-                    rng.GetBytes(randomBytes);
-
+                    rng.GetBytes(randomBytes); // Fill the array with random bytes
+                    int value = BitConverter.ToInt32(randomBytes, 0) & Int32.MaxValue; // Convert bytes to a positive integer
+                    numbers.Add(value % 1000); // Make sure the value is within the 0 - 999 range
                 }
             }
+        }
+        private static void RunBenchmark(Action benchmarkMethod, string methodName)
+        {
+            long startTime = DateTime.Now.Ticks;
+            benchmarkMethod();
+            long endTime = DateTime.Now.Ticks;
+            long elapsedTime = endTime - startTime;
+            Console.WriteLine($"{methodName} time (ticks): {elapsedTime}");
         }
     }
 }
