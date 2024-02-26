df8cc108d570143e201b1e87579e9c634762411e
Simon Onyszczak Hansen <simo401t@zbc.dk>
2024-02-26 11:23:00 +0100

    Add project files.

---
+++ Tilfældihederr/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilfældihederr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> randomNumber = RandomTest();
+            List<int> rngNumbers = RandomTest();
+
+            foreach (int number in randomNumber)
+            {
+               Console.WriteLine($"Random number: {number}");
+            }
+
+
+            foreach (int number in rngNumbers)
+            {
+                Console.WriteLine($"rng number: {number}");
+            }
+
+            Console.ReadLine();
+        }
+
+        static List<int> RandomTest() 
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 100; i++) 
+            {
+                numbers.Add(random.Next(0, 1000));
+            }
+            return numbers;
+        }
+        static List<int> RandomNumberGeneratorCreateTest()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0;i < 100;i++) 
+            {
+                byte[] randomBytes = new byte[4];
+                using (var rng = RandomNumberGenerator.Create())
+                {
+                    rng.GetBytes(randomBytes);
+
+                }
+            }
+        }
+    }
+}
